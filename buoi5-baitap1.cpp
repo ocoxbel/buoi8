d1112d84289b68a672ee0b3bc8d6e1db414f7f1d
ocoxbel <minh.caophuc2010@gmail.com>
2026-08-17 19:41:47 +0700

    Delete buoi5-baitap1.cpp

--- buoi5-baitap1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int a=34, b= 23, c= 11;
-    cout << "Tong la:" << a+b+c << endl;
-    cout << "Trung binh la:" << float(a+b+c)/3 << endl;
-    return 0;
-}
